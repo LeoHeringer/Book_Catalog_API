0a4c4e9e734195ab63e5f608ff873e28428a5bc4
LeoHeringer <leonardoheringer2016@hotmail.com>
2024-05-04 01:40:03 -0300

    models

--- rest/models.py
+++ rest/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.model):
+    
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200, null=False, blank=False)
+    author = models.CharField(max_length=200, null=False, blank=False)
+    launch = models.IntegerField(null=False, blank=False)
+    edition = models.IntegerField(null=False, blank=False)
+    type = models.CharField(max_length=200, null=False, blank=False)
+    gender = models.CharField(max_length=200, null=False, blank=False)
+    publisher = models.CharField(max_length=200, null=False, blank=False)
+    year_edition = models.IntegerField(null=False, blank=False)
+    edition_number = models.IntegerField(null=False, blank=False)
