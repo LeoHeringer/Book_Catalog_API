99f3eba282786ba6b5168ccdc908edff0df73628
LeoHeringer <leonardoheringer2016@hotmail.com>
2024-05-04 02:29:53 -0300

    mysql

--- rest/models.py
+++ rest/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Book(models.model):
+class Book(models.Model):
     
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200, null=False, blank=False)
