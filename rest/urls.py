289646aafbc993d3af607226a3805a481aee16f0
LeoHeringer <leonardoheringer2016@hotmail.com>
2024-05-06 14:18:35 -0300

    post

--- rest/urls.py
+++ rest/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
 
+from . import views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/create-book', views.create_book),
 ]
--- rest/views.py
+++ rest/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes, renderer_classes
+from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework import status
 
-# Create your views here.
+from core.models import *
+
+@api_view(['POST'])
+def create_book(request):
+    
+    name = request.data.get("name")
+    author = request.data.get("author")
+    launch = request.data.get("launch")
+    edition = request.data.get("edition")
+    type =  request.data.get("typ")
+    gender =  request.data.get("gender")
+    publisher = request.data.get("publisher")
+    year_edition = request.data.get("year")
+    edition_number = request.data.get("edition_number")
+    
+    if not name:
+        return Response({"message": "name invalid"}, status=status.http_400_BAd_REQUEST)
+    
+    if not author:
+        return Response({"message": "name invalid"}, status=status.http_400_BAd_REQUEST)
+    
+    if not launch:
+        return Response({"message": "name invalid"}, status=status.http_400_BAd_REQUEST)
+
+    if not edition:
+        return Response({"message": "name invalid"}, status=status.http_400_BAd_REQUEST)
+
+    if not type:
+        return Response({"message": "name invalid"}, status=status.http_400_BAd_REQUEST)
+
+    if not gender:
+        return Response({"message": "name invalid"}, status=status.http_400_BAd_REQUEST)
+
+    if not publisher:
+        return Response({"message": "name invalid"}, status=status.http_400_BAd_REQUEST)
+
+    if not year_edition:
+        return Response({"message": "name invalid"}, status=status.http_400_BAd_REQUEST)
+
+    if not edition_number:
+        return Response({"message": "name invalid"}, status=status.http_400_BAd_REQUEST)
+    
+    if Book.objects.filter(name=name, author=author):
+        return Response({"message":"exists"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    if name and author:
+        Book.objects.create(name=name, author=author)
+        return Response({"message":"create"}, status=status.HTTP_200_OK)
+    
+    return Response({"message":"invalid"}, status=status.HTTP_400_BAD_REQUEST)
